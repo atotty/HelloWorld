bcb2e48a05b612ea4c97475aa95e2721424da71c
JasonSaber <jsaber19@windwardschool.org>
2018-09-13 10:17:15 -0700

    initial 2

---
+++ src/GreetingsEarth.java
@@ -0,0 +1,8 @@
+public class GreetingsEarth {
+    public static void main(String[] args){
+        System.out.println("Greetings Earth!");
+        System.out.println("This is a new feature");
+    }
+
+
+}
